2d0a30ad23b7b5f819de3dcbf3863255ff98f635
AashiiMittal <aashimittal2004@gmail.com>
2025-03-07 21:19:16 +0530

    Logging Levels, patterns etc

--- src/main/java/com/example/employee_payroll_app/EmployeePayrollAppApplication.java
+++ src/main/java/com/example/employee_payroll_app/EmployeePayrollAppApplication.java
@@ -1,14 +1,19 @@
 package com.example.employee_payroll_app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
 //		System.out.println("hello");
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment",
+				context.getEnvironment().getProperty("spring.profiles.active"));
 	}
 
 }
